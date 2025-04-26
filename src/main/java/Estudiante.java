b05c261d66638a547ffa2f2cb59fe66b0e566817
Joel Tapia <jtapiasuarez49@gmail.com>
2025-04-26 12:15:55 -0500

    Segundo cambio

---
+++ src/main/java/Estudiante.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jtapi
+ */
+public class Estudiante {
+    public class Estudiante {
+    private String nombre;
+    private int edad;
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
+}
